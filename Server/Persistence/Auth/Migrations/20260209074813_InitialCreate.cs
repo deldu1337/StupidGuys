6aed6498c1dcc463a945bad0c1e27b8238a50a0d
JSH <deldu1337@naver.com>
2026-02-09 17:55:01 +0900

    [Fix] 프로세스 무중단 재기동 수정

--- Server/Persistence/Auth/Migrations/20260209074813_InitialCreate.cs
+++ Server/Persistence/Auth/Migrations/20260209074813_InitialCreate.cs
@@ -20,7 +20,8 @@ namespace Auth.Migrations
                     Password = table.Column<string>(type: "text", nullable: false),
                     Nickname = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: true),
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
-                    LastConnected = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                    LastConnected = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    SkinIndex = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
--- Server/StupidGuysServer/StupidGuysServer/obj/Release/net8.0/StupidGuysServer.AssemblyInfo.cs
+++ Server/StupidGuysServer/StupidGuysServer/obj/Release/net8.0/StupidGuysServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StupidGuysServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fbad80f3dd8b12140195279e48f127ddc638b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045008276fabae34b2bc162c4b7ab5454206dbfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StupidGuysServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StupidGuysServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
