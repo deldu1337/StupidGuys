a7b81f5c2ad7198e050a5bbd7d145c707a5416ad
JSH <77719450+deldu1337@users.noreply.github.com>
2026-02-10 18:12:52 +0900

    Force matchmaking to use fixed lobby id 1

--- Server/StupidGuysServer/StupidGuysServer/Services/LobbiesManager.cs
+++ Server/StupidGuysServer/StupidGuysServer/Services/LobbiesManager.cs
@@ -1,31 +1,28 @@
 ﻿using StupidGuysServer.Models;
 using System.Collections.Concurrent;
-using System.Linq;
 
 namespace StupidGuysServer.Services
 {
     public class LobbiesManager
     {
+        private const int FixedLobbyId = 1;
+
         private readonly ConcurrentDictionary<int, Lobby> _lobbies = new();
-        private int _nextLobbyId = 1;
-        private readonly object _idLock = new object();
 
         public Lobby? FindAvailableLobby()
         {
-            return _lobbies.Values.FirstOrDefault(lobby => !lobby.IsFull && !lobby.IsMatchFinalized);
+            if (_lobbies.TryGetValue(FixedLobbyId, out var lobby) && !lobby.IsFull && !lobby.IsMatchFinalized)
+            {
+                return lobby;
+            }
+
+            return null;
         }
 
         public Lobby CreateLobby(int maxPlayers)
         {
-            int lobbyId;
-            lock (_idLock)
-            {
-                lobbyId = _nextLobbyId++;
-            }
-
-            var lobby = new Lobby(lobbyId, maxPlayers);
-            _lobbies[lobbyId] = lobby;
-
+            var lobby = new Lobby(FixedLobbyId, maxPlayers);
+            _lobbies[FixedLobbyId] = lobby;
             return lobby;
         }
 
@@ -37,22 +34,26 @@ namespace StupidGuysServer.Services
 
         public Lobby? RemovePlayerFromAllLobbies(string connectionId)
         {
-            foreach (var lobby in _lobbies.Values)
+            if (!_lobbies.TryGetValue(FixedLobbyId, out var lobby))
             {
-                if (lobby.IsMatchFinalized)
+                return null;
+            }
+
+            if (lobby.IsMatchFinalized)
+            {
+                return null;
+            }
+
+            if (lobby.TryRemoveMember(connectionId, out int remainCount))
+            {
+                if (remainCount == 0)
                 {
-                    continue;
+                    _lobbies.TryRemove(FixedLobbyId, out _);
                 }
 
-                if (lobby.TryRemoveMember(connectionId, out int remainCount))
-                {
-                    if (remainCount == 0)
-                    {
-                        _lobbies.TryRemove(lobby.Id, out _);
-                    }
-                    return lobby;
-                }
+                return lobby;
             }
+
             return null;
         }
 
